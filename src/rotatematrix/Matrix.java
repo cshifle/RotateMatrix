472a770b1a8315962100ba03b6d3104e9f5ae511
cshifle <cshiffle1991@gmail.com>
2015-10-26 10:27:12 -0400

    Add new files to repository

---
+++ src/rotatematrix/Matrix.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rotatematrix;
+
+/**
+ *
+ * @author Christopher
+ */
+public class Matrix {
+    int [][] theMatrix;     //declare the 2d matrix
+    public Matrix(){
+        
+        theMatrix = new int [5][5];
+        //Initialize 2d matrix with values. Each row will have the value of the row number
+        for(int i = 0; i< theMatrix.length; i++){
+            for(int j =0; j < theMatrix[i].length; j++){
+                theMatrix[i][j] = i+1;
+            }
+            
+        }
+    }
+    
+    void print(){
+        System.out.println("This is the original Matrix....");
+        for(int i = 0; i < theMatrix.length; i++){
+            for(int j = 0; j <theMatrix[i].length; j++){
+                System.out.print(theMatrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    
+    void rotate45(){
+        
+    }
+    
+    void rotate90(){
+        
+    }
+    
+    void rotate135(){
+        
+        
+    }
+}
---
+++ src/rotatematrix/RotateMatrix.java
@@ -0,0 +1,26 @@
+
+
+package rotatematrix;
+
+/**
+ *
+ * @author Christopher
+ */
+public class RotateMatrix {
+
+
+    public static void main(String[] args) {
+        
+        //Creates a new matrix object (the matrix values are hard coded)
+        Matrix m = new Matrix();
+        m.print(); //print the original matrix
+        
+        
+        
+        
+
+        
+        
+    }
+    
+}
